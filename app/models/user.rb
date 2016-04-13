eaa93b16478486e4e13df33954a78e9f0a5e12d1
weagleweagle <eddyfabery@gmail.com>
2016-04-13 19:39:26 -0400

    oauth fix, add temp email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
-  # TEMP_EMAIL_PREFIX = 'change@me'
-  # TEMP_EMAIL_REGEX = /\Achange@me/
+  TEMP_EMAIL_PREFIX = 'change@me'
+  TEMP_EMAIL_REGEX = /\Achange@me/
 
   devise :database_authenticatable, :registerable, :recoverable,
     :rememberable, :trackable, :validatable, :omniauthable
 
-  # validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
+  validates_format_of :email, :without => TEMP_EMAIL_REGEX, on: :update
 
   def self.find_for_oauth(auth, signed_in_resource = nil)
 
@@ -24,9 +24,9 @@ class User < ActiveRecord::Base
       # Get the existing user by email if the provider gives us a verified email.
       # If no verified email was provided we assign a temporary email and ask the
       # user to verify it on the next step via UsersController.finish_signup
-      # email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
-      # email = auth.info.email if email_is_verified
-      # user = User.where(:email => email).first if email
+      email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
+      email = auth.info.email if email_is_verified
+      user = User.where(:email => email).first if email
 
       # Create the user if it's a new registration
       if user.nil?
