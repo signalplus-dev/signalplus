ca27052204524b5903d2462f38cf50ed30d87cdf
Ricardo Quinones <rq.project.signal@gmail.com>
2016-04-24 11:54:50 -0400

    Increases time between sidetiq recurrences

--- app/workers/tiqs/twitter_cron_job.rb
+++ app/workers/tiqs/twitter_cron_job.rb
@@ -5,11 +5,11 @@ class TwitterCronJob
   sidekiq_options :retry => false
 
   recurrence backfill: false do
-    hourly.minute_of_hour(*(0...60).to_a)
+    hourly.minute_of_hour(*(0...60).step(2).to_a)
   end
 
   def perform(last_occurrence, current_occurrence)
-    sleep Time.at(current_occurrence).min
+    sleep 1
 
     Brand.select(:id).find_each do |b|
       TwitterListener.process_brand_messages(b.id)
