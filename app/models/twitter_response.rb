716d5ce0906e7380d6a8a500db64645af01c0164
efabery-handy <eddy@handy.com>
2016-05-21 14:40:28 -0400

    Removes unused code

--- app/models/twitter_response.rb
+++ app/models/twitter_response.rb
@@ -30,45 +30,6 @@ class TwitterResponse < ActiveRecord::Base
     def direct_messages
       where(response_type: ResponseType::DIRECT_MESSAGE)
     end
-
-    private
-
-    # @return [String] A valid, sanitized sql statement for the
-    def base_sql_statement(date, from, hashtag)
-      arel_table
-        .create_insert
-        .tap { |im| im.insert(base_insert_attributes(date, from, hashtag)) }
-        .to_sql
-    end
-
-    # @return [String] The base part of the insert statement, containing the all column declarations
-    #                  that should be inserted.
-    def base_insert_statement(date, from, hashtag)
-      base_sql_statement(date, from, hashtag)[/^.+VALUES\s/]
-    end
-
-    # @return [Array] A base array containing all the similar insert portions of the mass insert statement
-    def base_insert_values(date, from, hashtag)
-      base_sql_statement(date, from, hashtag)[/(?<=VALUES\s\()[^\)]+/].split(', ')
-    end
-
-    # @return [Hash] A hash containing all the relevant insert data for the mass insert statement
-    def base_insert_attributes(date, from, hashtag)
-      base_twitter_response(date, from, hashtag).arel_mass_insert_attributes_for_create
-    end
-
-    # @return [TwitterResponse]
-    def base_twitter_response(date, from, hashtag)
-      current_time = Time.current
-
-      new do |tr|
-        tr.date       = date
-        tr.from       = from
-        tr.hashtag    = hashtag
-        tr.created_at = current_time
-        tr.updated_at = current_time
-      end
-    end
   end
 
   # @return [Hash] A hash containing all the relevant Arel data to create the insert statment for
