38af7901ab4b40a48f2b7feb9860b46174b6478e
efabery-handy <eddy@handy.com>
2016-05-26 20:21:49 -0400

    remove minimagic we don't need

--- app/models/promotional_tweet.rb
+++ app/models/promotional_tweet.rb
@@ -1,17 +1,10 @@
 class PromotionalTweet < ActiveRecord::Base
   belongs_to :listen_signal
 
-  # Resize image
-  has_attatched_file :image, styles: {
-    medium: '300x300>'
+  has_attached_file :image, :styles => {
+    :medium => "300x300#",
+    :thumb => "200x200#"
   }
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png"] }
 
-  # Validate the attached image is image/jpg, image/png, etc
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
-
-  def save_image
-  end
-
-  def get_image
-  end
 end
