21eafd1e8ca1e7c9006d97aa0dca3dd779b21269
Ricardo Quinones <rq.project.signal@gmail.com>
2016-07-24 19:56:15 -0400

    Allows for upgrading/downgrading subscription plans and for subscription cancellations

--- spec/factories/subscription_plans.rb
+++ spec/factories/subscription_plans.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: subscription_plans
+#
+#  id                 :integer          not null, primary key
+#  amount             :integer
+#  name               :string
+#  number_of_messages :integer
+#  currency           :string
+#  provider           :string
+#  provider_id        :string
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
+
 FactoryGirl.define do
   factory :subscription_plan do
     provider 'Stripe'
@@ -6,5 +21,11 @@ FactoryGirl.define do
     name 'Basic'
     currency 'usd'
     number_of_messages 5000
+
+    trait :advanced do
+      name 'Advanced'
+      provider_id 'advanced'
+      number_of_messages 15000
+    end
   end
 end
