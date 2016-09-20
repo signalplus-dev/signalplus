43185bd67c086326150b75dd64aa50ef8115eee2
weagleweagle <eddyfabery@gmail.com>
2016-09-19 23:19:20 -0400

    clean up unnecesary shit for navbat

--- app/assets/javascripts/components/dashboard/navigation/navigation.jsx
+++ app/assets/javascripts/components/dashboard/navigation/navigation.jsx
@@ -9,27 +9,7 @@ import Tabs from './tabs.jsx';
 class Navigation extends Component {
   constructor(props) {
     super(props);
-    this.handleTabs     = this.handleTabs.bind(this);
-    this.handleTabClick = this.handleTabClick.bind(this);
     this.handleSignal   = this.handleSignal.bind(this);
-    this.state = {
-      tabList: [
-        {
-          id: 1,
-          name: 'SIGNALS',
-          paneId: 'signals',
-          active: true,
-        },
-        {
-          id: 2,
-          name: 'CREATE NEW',
-          paneId: 'templates',
-          active: false,
-        }
-      ],
-      editSignal: '',
-      templateType: '',
-    };
   }
 
   componentWillMount() {
@@ -37,45 +17,10 @@ class Navigation extends Component {
     if (!listenSignals.loaded) dispatch(fetchListenSignalsData());
   }
 
-  handleTabs(tab) {
-    const newTabs = [
-      {
-        id: 1,
-        name: 'SIGNALS',
-        paneId: 'signals',
-        active: false,
-      },
-      {
-        id: 2,
-        name: 'CREATE NEW',
-        paneId: 'templates',
-        active: false,
-      }
-    ];
-
-    newTabs.push({
-      id: newTabs.length + 1,
-      name: tab.name,
-      paneId: tab.paneId,
-      active: true,
-    });
-
-    this.setState({tabList: newTabs})
-  }
-
   handleSignal(key, value) {
     var obj = {};
     obj[key] = value;
     this.setState(obj);
-  }
-
-  handleTabClick(tabId) {
-    const newTabs = this.state.tabList.map((t) => {
-      t.active = t.id == tabId;
-      return t;
-    });
-
-    this.setState({ tabList: newTabs });
   }
 
   render() {
