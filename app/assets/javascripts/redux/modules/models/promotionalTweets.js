5a64a776b295e27d09510a780be84b5ef1c55509
weagleweagle <eddyfabery@gmail.com>
2016-09-29 23:50:22 -0400

    fix styling and connect promotional to the store, use restapi auth request function instead

--- app/assets/javascripts/redux/modules/models/promotionalTweets.js
+++ app/assets/javascripts/redux/modules/models/promotionalTweets.js
@@ -1,12 +1,17 @@
 import { get } from 'lodash';
 import { handleActions } from 'redux-actions';
 import { normalizeListenSignalsResponse } from '../../../util/normalize.js';
+import Endpoints from '../../../util/endpoints.js';
 import {
   LISTEN_SIGNALS_REQUEST,
   LISTEN_SIGNALS_REQUEST_SUCCESS,
   LISTEN_SIGNALS_REQUEST_FAIL,
 } from './listenSignals.js';
 
+
+const PROMOTION_SIGNAL_POST_REQUEST = 'signalplus/promotionalSignal/REQUEST';
+const PROMOTION_SIGNAL_POST_REQUEST_SUCCESS = 'signalplus/promotionalSignal/REQUEST_SUCESS';
+const PROMOTION_SIGNAL_POST_REQUEST_FAIL = 'signalplus/promotionalSignal/REQUEST_FAIL';
 
 /*
 * Initial State
@@ -40,4 +45,38 @@ export const reducer = handleActions({
     loading: false,
     loaded: false,
   }),
+
+  [PROMOTION_SIGNAL_POST_REQUEST]: (state, action) => ({
+    ...state,
+    data: get(normalizedListenSignalResponse(action.payload), 'entities.promotionalTweet', {}),
+    loading: false,
+    loaded: true,
+  }),
+
+  [PROMOTION_SIGNAL_POST_REQUEST_SUCCESS]: (state, action) => ({
+    ...state,
+    data: get(normalizedListenSignalResponse(action.payload), 'entities.promotionalTweet', {}),
+    loading: false,
+    loaded: true,
+  }),
+
+  [PROMOTION_SIGNAL_POST_REQUEST_FAIL]: (state, action) => ({
+    ...state,
+    error: action.payload,
+    loading: false,
+    loaded: true,
+  }),
 }, initialState);
+
+export const addPromotionalTweetData = (payload) => {
+  return createRequestAction({
+    endpoint: Endpoints.PROMOTIONAL_SIGNAL_INDEX,
+    method: 'POST',
+    body: JSON.stringify(payload),
+    types: [
+      PROMOTION_SIGNAL_POST_REQUEST,
+      PROMOTION_SIGNAL_POST_REQUEST_SUCCESS,
+      PROMOTION_SIGNAL_POST_REQUEST_FAIL,
+    ],
+  })
+}
