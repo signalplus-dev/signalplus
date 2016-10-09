47d9ad3b26c297f2a1b145642708fb648c5bb430
Ricardo Quinones <rq.project.signal@gmail.com>
2016-10-09 08:53:02 -0400

    Conditionally shows dev tools

--- app/assets/javascripts/redux/configureStore.js
+++ app/assets/javascripts/redux/configureStore.js
@@ -2,6 +2,9 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 import thunk from 'redux-thunk';
 import rootReducer from 'redux/modules/index.js';
+import { isDev } from 'util/env.js';
+
+const showDevTools = isDev() && !!window.devToolsExtension;
 
 export default function configureStore(initialState = {}) {
   return createStore(
@@ -12,7 +15,7 @@ export default function configureStore(initialState = {}) {
         apiMiddleware,
         thunk
       ),
-      window.devToolsExtension() // TODO - Make this not available in prod environment
+      showDevTools ? window.devToolsExtension() : (arg) => arg
     )
   );
 }
