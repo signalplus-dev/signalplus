d07c79825c04a278a5055de65053d05ce08bf027
weagleweagle <eddyfabery@gmail.com>
2016-10-23 21:18:46 -0400

    fix timezone

--- app/assets/javascripts/components/forms/timezoneDropdown.jsx
+++ app/assets/javascripts/components/forms/timezoneDropdown.jsx
@@ -4,7 +4,6 @@ import moment from 'moment-timezone';
 import TimezonePicker from 'react-bootstrap-timezone-picker';
 import _ from 'lodash';
 
-<<<<<<< HEAD
 const getTimezoneWithLabels = () => {
   const timezones = moment.tz.names();
   const timezonesWithLabels= {};
@@ -15,27 +14,7 @@ const getTimezoneWithLabels = () => {
   });
 
   return timezonesWithLabels;
-=======
-const ACTIVE_TIMEZONE_LIST = [
-  'America/Los_Angeles', 
-  'America/Denver', 
-  'America/Chicago',
-  'America/New_York',
-]
-
-const getActiveTimezone = () => {
-  const timezones = moment.tz.names();
-  const activeTimezones = {};
-
-  timezones.map(function(tz) {
-    if (ACTIVE_TIMEZONE_LIST.includes(tz)) {
-      const label = " (GMT" + moment.tz(tz).format('Z')+") " + tz;
-      activeTimezones[label] = tz;
-    }
-  });
-
-  return activeTimezones;
-} 
+}
 
 class TimezoneDropdown extends PureComponent {
   constructor(props) {
