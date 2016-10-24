d0d2b0210750903dc6fc5a52745a1281c229defb
weagleweagle <eddyfabery@gmail.com>
2016-10-23 22:53:52 -0400

    clean up tz dropdown

--- app/assets/javascripts/components/forms/timezoneDropdown.jsx
+++ app/assets/javascripts/components/forms/timezoneDropdown.jsx
@@ -1,20 +1,6 @@
 import React, { PureComponent } from 'react';
 import { Field } from 'redux-form';
-import moment from 'moment-timezone';
 import TimezonePicker from 'react-bootstrap-timezone-picker';
-import _ from 'lodash';
-
-const getTimezoneWithLabels = () => {
-  const timezones = moment.tz.names();
-  const timezonesWithLabels= {};
-
-  timezones.map(function(tz) {
-    const label = " (GMT" + moment.tz(tz).format('Z')+") " + tz;
-    timezonesWithLabels[label] = tz;
-  });
-
-  return timezonesWithLabels;
-}
 
 class TimezoneDropdown extends PureComponent {
   constructor(props) {
@@ -24,10 +10,6 @@ class TimezoneDropdown extends PureComponent {
 
   onChange(e) {
     this.props.input.onChange(e);
-  }
-
-  findTimezoneLabel(timezones, tz) {
-    return _.findKey(timezones, (tzLabel) => (tzLabel.indexOf(tz)));
   }
 
   render() {
@@ -40,17 +22,15 @@ class TimezoneDropdown extends PureComponent {
       meta,
       ...props,
     } = this.props;
-    const timezones = getTimezoneWithLabels();
 
     return (
       <div className='tz-dropdown'>
         <TimezonePicker
           {...props}
-          defaultValue={this.findTimezoneLabel(timezones, input.value)}
+          value={input.value}
           absolute={false}
           placeholder='Select timezone...'
           onChange={this.onChange}
-          timezones={timezones}
         />
       </div>
     );
