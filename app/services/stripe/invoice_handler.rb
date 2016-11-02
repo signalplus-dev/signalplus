8c20f2bfd3437b043a83524f0a12a5a15f3e3a61
weagleweagle <eddyfabery@gmail.com>
2016-11-02 18:35:46 -0400

    invoice service quick fixes

--- app/services/stripe/invoice_handler.rb
+++ app/services/stripe/invoice_handler.rb
@@ -1,6 +1,6 @@
 class InvoiceHandler
 
-  # @param [JSON] Stripe Webhook Invoice Payload
+  # @param [Hash] Stripe Webhook Invoice Payload
   # @return [InvoiceHandler] 
   def initialize(invoice_data)
     @invoice_data= invoice_data
@@ -11,8 +11,7 @@ class InvoiceHandler
       brand:              get_brand,
       stripe_invoice_id:  @invoice_data.id,
       amount:             @invoice_data.amount_due,
-      data:               @invoice_data,
-      paid_at:            Time.now.utc
+      data:               @invoice_data
     )
   end
 
@@ -26,6 +25,7 @@ class InvoiceHandler
   private
 
   def get_brand
-    PaymentHandler.find_by(token: @invoice_data.customer).try(:brand)
+    brand_id = PaymentHandler.where(token: @invoice_data.customer).pluck(:brand_id).first
+    raise StandardError.new('Could not PaymentHandler for that customer')  unless brand_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161029181742) do
+ActiveRecord::Schema.define(version: 20161102223126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,7 +45,7 @@ ActiveRecord::Schema.define(version: 20161029181742) do
 
   create_table "invoices", force: :cascade do |t|
     t.string   "stripe_invoice_id"
-    t.integer  "brand_id"
+    t.integer  "brand_id",          null: false
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
     t.datetime "paid_at"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20161029181742) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_payment_handlers_on_brand_id", using: :btree
+    t.index ["token"], name: "index_payment_handlers_on_token", using: :btree
   end
 
   create_table "promotional_tweets", force: :cascade do |t|
