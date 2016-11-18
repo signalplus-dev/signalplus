78a04d683e1f471e2b3b6527bee4aab77947eeb6
weagleweagle <eddyfabery@gmail.com>
2016-11-17 22:24:59 -0500

    fix worker specs

--- spec/workers/kiqs/email_remove_subscription_worker_spec.rb
+++ spec/workers/kiqs/email_remove_subscription_worker_spec.rb
@@ -5,15 +5,16 @@ describe EmailRemoveSubscriptionWorker do
   let(:worker)           { described_class.new }
   let(:user)             { create(:user, brand: brand) }
   let(:list_id)          { '111' }
-  let(:encrypted_email)  { user.email }
+  let(:encrypted_email)  { user.encrypted_email }
   let(:mock_client)      { object_spy('mock_client') }
 
   before {
+    allow(ENV).to receive(:[]).and_return(list_id)
     allow(Gibbon::Request).to receive(:new).and_return(mock_client)
   }
 
   it 'subscribes user in MailChimp list' do
-    worker.perform(list_id, encrypted_email)
+    worker.perform(encrypted_email)
 
     expect(mock_client).to have_received(:lists).with(list_id)
     expect(mock_client).to have_received(:members).with(encrypted_email)
