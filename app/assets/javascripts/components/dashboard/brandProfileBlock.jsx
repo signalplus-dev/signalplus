197849b84378f24ecbd196030d33922020238463
Ricardo Quinones <rq.project.signal@gmail.com>
2016-11-19 15:40:11 -0500

    Fixes issue with profile image not being the right http scheme

--- app/assets/javascripts/components/dashboard/brandProfileBlock.jsx
+++ app/assets/javascripts/components/dashboard/brandProfileBlock.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import URL from 'url';
 import Loader from 'components/loader.jsx';
 
 function BrandProfileBlock({ brand }) {
 
   if (!brand.loaded) return <Loader />;
   const brandInfo = _.get(brand, 'data', {});
+  const profileImageUrl = URL.parse(brandInfo.profile_image_url || '')
+  const profileImageSrc = `//${profileImageUrl.host || ''}${profileImageUrl.path || ''}`
 
   return (
     <div className="brand-profile-block">
-      <img className="brand-profile-img" src={brandInfo.profile_image_url} alt="Brand profile image"/>
+      <img className="brand-profile-img" src={profileImageSrc} alt="Brand profile image"/>
       <div className="brand-profile-name">
         <h3>{brandInfo.name}</h3>
         <p>{`@${brandInfo.user_name}`}</p>
