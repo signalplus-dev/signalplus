5df8f07c0ad3a60e9313a589f356397d26da0516
weagleweagle <eddyfabery@gmail.com>
2016-11-27 14:01:50 -0500

    close account

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,6 @@ class User < ActiveRecord::Base
         # user to verify it on the next step via UsersController.finish_signup
         email_is_verified = auth.info.email && (auth.info.verified || auth.info.verified_email)
         email = auth.info.email if email_is_verified
-        user = User.with_deleted.where(:email => email).first if email
 
         # Create the user if it's a new registration
         if user.nil?
