c1f9cc017102f92f4699f72e96d555b9a19bada3
weagleweagle <eddyfabery@gmail.com>
2016-12-26 18:56:43 -0500

    homepage links

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
   root to: 'pages#show', id: 'home'
 
   get 'users/index'
-  match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
+  # match '/users/:id/finish_signup' => 'users#finish_signup', via: [:get, :patch], :as => :finish_signup
 
   resources :dashboard, only: [:index]
 
