c6d23563647c3dcd107d7700bcee9cee5f7d4fda
weagleweagle <eddyfabery@gmail.com>
2016-12-27 18:20:15 -0500

    rescue errors

--- app/services/mailer/transactional_email.rb
+++ app/services/mailer/transactional_email.rb
@@ -1,4 +1,4 @@
-require "mandrill"
+require 'mandrill'
 
 class TransactionalEmail
   attr_reader :mandrill, :template_name, :brand, :merge_params, :message
@@ -10,7 +10,7 @@ class TransactionalEmail
   end
 
   def initialize(template_name, brand)
-    @mandrill = Mandrill::API.new(ENV["MANDRILL_API_KEY"])
+    @mandrill = Mandrill::API.new(ENV['MANDRILL_API_KEY'])
     @template_name = template_name
     @brand = brand
     @to = brand.twitter_admin.email
@@ -19,47 +19,53 @@ class TransactionalEmail
   end
 
   def send
-    @mandrill.messages.send_template(@template_name, @message)
+    begin
+      @mandrill.messages.send_template(@template_name, @message)
+    rescue Mandrill::Error => e
+      Rollbar.error(e)
+    end
   end
 
   private
 
   def get_message
     {
-      "tags": @template_name,
-      "merge_vars": [{
-        "rcpt": @to,
-        "vars": @email_attributes
+      'tags': @template_name,
+      'merge_vars': [{
+        'rcpt': @to,
+        'vars': @email_attributes
       }],
-     "merge_language": "mailchimp",
-     "merge": true,
-     "headers": {"Reply-To"=>"info@signalplus.com"},
-     "to": [{
-        "type": "to",
-        "email": @to
+     'merge_language': 'mailchimp',
+     'merge': true,
+     'headers': {
+        'Reply-To'=>'info@signalplus.com'
+      },
+     'to': [{
+        'type': 'to',
+        'email': @to
       }],
-     "track_opens": true,
-     "async": true
+     'track_opens': true,
+     'async': true
     }
   end
 
   def get_email_attributes
     default_opt = [
       {
-        "name": "BRAND_HASHTAG",
-        "content": @brand.name
+        'name': 'BRAND_HASHTAG',
+        'content': @brand.name
       },
       {
-        "name": "PLAN_NAME",
-        "content": @brand.subscription_plan_name
+        'name': 'PLAN_NAME',
+        'content': @brand.subscription_plan_name
       }
     ]
 
     date_attr = get_plan_attr
     if date_attr.present?
       default_opt << {
-        "name": "PLAN_DATE",
-        "content": date_attr
+        'name': 'PLAN_DATE',
+        'content': date_attr
       }
     end
 
