adeb0221fffd2e695c749249d23e5fcf1835c89f
weagleweagle <eddyfabery@gmail.com>
2016-12-28 15:48:10 -0500

    spec fix

--- spec/services/mailer/transactional_email_spec.rb
+++ spec/services/mailer/transactional_email_spec.rb
@@ -7,21 +7,23 @@ describe TransactionalEmail do
     let!(:identity)      { create(:identity) }
     let!(:subscription) { create(:subscription, brand: brand)}
 
-    before do
+    before(:each) do
       allow(Mandrill::API).to receive(:new).and_call_original
     end
 
-  # RESPONSE SHOULD LOOK LIKE THIS
-  #   [{"email"=>"eddyfabery@gmail.com",
-  #     "status"=>"sent",
-  #     "_id"=>"86566aed99df4dbdbcfd197da7fb9b04",
-  #     "reject_reason"=>nil}]
-
     context 'welcome email' do
+    # EXAMPLE RESPONSE
+    # [
+    #  {"email"=>"eddyfabery@gmail.com",
+    #   "status"=>"sent",
+    #   "_id"=>"86566aed99df4dbdbcfd197da7fb9b04",
+    #   "reject_reason"=>nil}
+    # ]
+
       it 'sends welcome email template' do
         VCR.use_cassette 'mandrill_response' do
           response = described_class.welcome(brand).send
-          #expect(response[0]['reject_reason').to be_nil
+          expect(response[0]['reject_reason']).to be_nil
         end
       end
     end
