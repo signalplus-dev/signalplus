e243ee9c5080663a9e7befb302457aa914c093cf
weagleweagle <eddyfabery@gmail.com>
2017-01-21 17:05:23 -0500

    change panel text according to signal type

--- app/assets/javascripts/components/signalsPanel/signalWidgets.jsx
+++ app/assets/javascripts/components/signalsPanel/signalWidgets.jsx
@@ -20,7 +20,10 @@ function renderSignalWidgets(signals) {
       <Link to={`/dashboard/signals/${id}`} className={signalClasses} key={`signal_${id}`}>
         <SignalIcon className='panel-icon' src={window.__IMAGE_ASSETS__[`icons${_.capitalize(signal_type)}Svg`]} />
         <div className='panel-header'>{`#${name}`}</div>
-        <div className='panel-text'>Send your users a special offer every time they send a custom hashtag</div>
+        <div className='panel-text'>
+          { signal_type == 'offers' && 'Send your users a special offer every time they send a custom hashtag'}
+          { signal_type == 'custom' && 'Send a summary of your location or event each day a follower uses a custom hashtag'}
+        </div>
 
         <div className='panel-status'>
           <div className='signal-status'>
